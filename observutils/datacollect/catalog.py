02ebbb73997482228ec94edd603a86b68567f432
Patrick Wells <pwells@ucdavis.edu>
2020-11-09 12:27:50 -0800

    Added I/O in fits format

--- observutils/datacollect/catalog.py
+++ observutils/datacollect/catalog.py
@@ -1,4 +1,7 @@
 import os
+from astropy.table import Table
+from astropy.io import  fits
+
 
 class catalog:
     def __init__(self, url):
@@ -16,22 +19,38 @@ class catalogData:
 
     """
 
-    def __init__(self, data, metadata, **kwargs):
+    def __init__(self, data, metadata, format='astropy', **kwargs):
         self.data = data
         self.meta = metadata
+        self.format = format
 
-    def toFile(self, fname, format='pickle'):
+    def toFile(self, fname, outformat='fits', savemeta=False):
         """
         Writes the catalog data to a file to be used later.
         ---------------
         params:
         fname: string, output file name
-        format: output format, currently only pickle is supported
+        format: output format, currently fits or pickle
         
         """
-        if format == 'pickle':
-            import pickle
-            pickle.dump(self, open(fname, 'wb'))
+        if not savemeta:
+            if outformat == 'pickle':
+                import pickle
+                pickle.dump(self, open(fname, 'wb'))
+            if outformat == 'fits':
+                if self.format == 'astropy':
+                    if fname.endswith('fits'):
+                        self.data.write(fname)
+                    else:
+                        self.data.write('.'.join([fname, 'fits']))
+                elif self.format == 'pandas':
+                    fits_data = Table.from_pandas(self.data)
+                    if fname.endswith('fits'):
+                        self.data.write(fname)
+                    else:
+                        self.data.write('.'.join([fname, '.fits']))
+
+
 
 def openCatalogData(fname, **kwargs):
     """
@@ -45,11 +64,15 @@ def openCatalogData(fname, **kwargs):
         print("ERROR: file {} does not exist".format(fname))
         return
 
-    try:
-        import pickle
-        data = pickle.load(open(fname, 'rb'))
-    except:
-        print("Error: Currently only pickled data is supported")
+    if fname.endswith('.fits'):
+        raw_data = fits.open(fname)
+        data = catalogData(Table(raw_data[1].data), None, 'astropy')
+    else:
+        try:
+            import pickle
+            data = pickle.load(open(fname, 'rb'))
+        except:
+            print("Error: Unable to read file {}".format(fname))
 
     if type(data) != type(catalogData(None, None)):
         print("Error: file {} does not contain catalog data".format(fname))
